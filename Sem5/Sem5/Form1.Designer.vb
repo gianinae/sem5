4b23eaf04a273c963b9a104ac73b1c98a0e57d61
Elias <Elias@GIANINA>
2017-02-09 21:32:37 -0600

    sem5listo

---
+++ Sem5/Sem5/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TxtPalabra = New System.Windows.Forms.TextBox()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Btn1 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnIguala = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TxtPalabra
+        '
+        Me.TxtPalabra.Location = New System.Drawing.Point(83, 25)
+        Me.TxtPalabra.Name = "TxtPalabra"
+        Me.TxtPalabra.Size = New System.Drawing.Size(100, 20)
+        Me.TxtPalabra.TabIndex = 0
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Location = New System.Drawing.Point(150, 226)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.Size = New System.Drawing.Size(100, 20)
+        Me.txtResultado.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(118, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'Btn1
+        '
+        Me.Btn1.Location = New System.Drawing.Point(12, 51)
+        Me.Btn1.Name = "Btn1"
+        Me.Btn1.Size = New System.Drawing.Size(75, 23)
+        Me.Btn1.TabIndex = 4
+        Me.Btn1.Text = "Invierte"
+        Me.Btn1.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(175, 51)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Busca Letra"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnIguala
+        '
+        Me.btnIguala.Location = New System.Drawing.Point(12, 81)
+        Me.btnIguala.Name = "btnIguala"
+        Me.btnIguala.Size = New System.Drawing.Size(75, 23)
+        Me.btnIguala.TabIndex = 6
+        Me.btnIguala.Text = "Iguala"
+        Me.btnIguala.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(262, 258)
+        Me.Controls.Add(Me.btnIguala)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Btn1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.TxtPalabra)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TxtPalabra As TextBox
+    Friend WithEvents txtResultado As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Btn1 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnIguala As Button
+End Class
---
+++ Sem5/Sem5/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    'esta función invierte un string
+    Function invierte(palabra As String) As String
+        Dim inv As String = StrReverse(palabra)
+        Return inv
+    End Function
+    'este boton llama a invertir
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+        txtResultado.Text = invierte(TxtPalabra.Text)
+    End Sub
+    'este boton llama a la pos de un char
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim vec() As String = Split(TxtPalabra.Text, ",")
+        txtResultado.Text = posicion(vec(0), (vec(1)))
+    End Sub
+    ' Funcion que  reciba un String y un char, y que devuelva la primera posición en la que se encuentra el Char. Si no está en la hilera retorna -1.
+    Function posicion(palabra As String, letra As String) As Integer
+        Dim pos As Integer = -1
+        Dim index As Integer = 1
+        Dim bandera As Boolean = False
+        While index <= palabra.Length And bandera = False
+            If letra = Mid(palabra, index, 1) Then
+                pos = index
+                bandera = True
+            End If
+            index += 1
+        End While
+
+        Return pos
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnIguala.Click
+        Dim vec() As String = Split(TxtPalabra.Text, ",")
+        txtResultado.Text = Iguala(vec(0), (vec(1)))
+    End Sub
+
+    Function Iguala(pal1 As String, pal2 As String) As Integer
+        Dim res = 0
+        If pal1.Length = pal2.Length Then
+            Dim index As Integer = 0
+            While pal1.Chars(index) = pal2.Chars(index) And index < pal1.Length - 1
+                index = index + 1
+            End While
+            If index = pal1.Length - 1 Then
+                res = 1
+            End If
+        End If
+        Return res
+    End Function
+End Class
